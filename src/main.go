8f0689445edb212d92bf2bd832fc509d4cf336da
kouhei-github <kohei0801nagamatsu@gmail.com>
2023-11-17 01:44:01 +0900

    Todoタスクと保存機能の追加
    このコミットにより、Todoタスクの追加と、それらのタスクをjsonファイルに保存する機能が追加された。todos.jsonとtodo.goの2つのファイルが追加されました。todos.json ファイルは todo タスクの情報を保存するために使われ、todo.go は todo タスクの構造（または項目）を定義し、定義された json ファイルにタスクを追加したり保存したりするメソッドを提供します。タスクは詳細、ステータス、作成時間、完了時間で構成される。

--- src/main.go
+++ src/main.go
@@ -1,7 +1,33 @@
-package src
+package main
 
-import "fmt"
+import (
+	"flag"
+	"fmt"
+	"github.com/kouhei-github/todo-list-cli-tool/todo"
+	"os"
+)
+
+const (
+	todoFile = "./storages/todos.json"
+)
 
 func main() {
-	fmt.Println("Hello Golang")
+	add := flag.Bool("add", false, "タスクを追加する")
+
+	flag.Parse()
+
+	todos := &todo.Todos{}
+
+	switch {
+	case *add:
+		todos.Add("Sample todo")
+		if err := todos.Store(todoFile); err != nil {
+			fmt.Fprintln(os.Stdout, err.Error())
+			os.Exit(0)
+		}
+	default:
+		fmt.Fprintln(os.Stdout, "invalid command")
+		os.Exit(0)
+	}
+
 }
